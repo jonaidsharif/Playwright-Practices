039a04c8409e44b2a1e12e281b6f21b6075109e1
Jonaid Bin Sharif <62690644+jonaidsharif@users.noreply.github.com>
2025-06-12 18:47:03 +0600

    lastly Finding Locators

--- tests/executingTestsInParallelMode.spec.js
+++ tests/executingTestsInParallelMode.spec.js
@@ -1,23 +1,23 @@
-// const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-// test.describe.parallel("Smoke Tests", () => {
+test.describe.parallel("Smoke Tests", () => {
 
-//     test.beforeEach(async ({ page }) => {
-//         await page.goto("/");
-//     })
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/");
+    })
 
-//     test("Simple click Test", async ({ page, browserName }) => {
-//         await page.click("text=Add/Remove Elements");
-//         await page.click("text=Add Element");
-//     })
+    test("Simple click Test", async ({ page, browserName }) => {
+        await page.click("text=Add/Remove Elements");
+        await page.click("text=Add Element");
+    })
 
-//     test("Duplicate Test 1", async ({ page }) => {
-//         await page.click("text=Add/Remove Elements");
-//         await page.click("text=Add Element");
-//     })
+    test("Duplicate Test 1", async ({ page }) => {
+        await page.click("text=Add/Remove Elements");
+        await page.click("text=Add Element");
+    })
 
-//     test("Duplicate Test 2", async ({ page }) => {
-//         await page.click("text=Add/Remove Elements");
-//         await page.click("text=Add Element");
-//     })
-// })
+    test("Duplicate Test 2", async ({ page }) => {
+        await page.click("text=Add/Remove Elements");
+        await page.click("text=Add Element");
+    })
+})
